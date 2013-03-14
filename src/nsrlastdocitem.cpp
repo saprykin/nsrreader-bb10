8096b65eeb4e0ef975792fe22ad1605576d917ff
Alexander Saprykin <xelfium@gmail.com>
2013-03-14 21:35:10 +0400

    Make UI in recent documents page more simpler

--- src/nsrlastdocitem.cpp
+++ src/nsrlastdocitem.cpp
@@ -17,45 +17,36 @@ NSRLastDocItem::NSRLastDocItem (bb::cascades::Container* parent) :
 	Container *rootContainer = Container::create().horizontal(HorizontalAlignment::Fill)
 						      .vertical(VerticalAlignment::Fill)
 						      .layout(StackLayout::create ())
-						      .background(ImagePaint (QUrl ("asset:///border-gray.amd")));
+						      .background(Color::fromRGBA (0.2, 0.2, 0.2, 1.0));
+	rootContainer->setLeftPadding (10);
+	rootContainer->setRightPadding (10);
+	rootContainer->setTopPadding (10);
+	rootContainer->setBottomPadding (10);
 
 	Container *imgContainer = Container::create().horizontal(HorizontalAlignment::Fill)
 					    .vertical(VerticalAlignment::Fill)
 					    .layout(DockLayout::create())
 					    .layoutProperties(StackLayoutProperties::create().spaceQuota(1.0));
 
-	imgContainer->setLeftPadding (2);
-	imgContainer->setRightPadding (3);
-	imgContainer->setTopPadding (10);
-	imgContainer->setBottomMargin (0);
-
 	_imageView = ImageView::create().horizontal(HorizontalAlignment::Fill)
 					.vertical(VerticalAlignment::Fill)
-					.scalingMethod(ScalingMethod::Fill)
-					.imageSource(QUrl ("asset:///background-black.png"));
-	_imageView->setBottomMargin (0);
+					.scalingMethod(ScalingMethod::Fill);
 
+	imgContainer->setBottomMargin (0);
 	imgContainer->add (_imageView);
 
 	_label = Label::create ().horizontal(HorizontalAlignment::Center)
 				 .vertical(VerticalAlignment::Center);
 	_label->textStyle()->setColor (Color::fromRGBA (0.8, 0.8, 0.8, 1.0));
 	_label->textStyle()->setFontSize (FontSize::XSmall);
-	_label->setTopMargin (0);
 
 	Container *labelContainer = Container::create().horizontal(HorizontalAlignment::Fill)
 						       .vertical(VerticalAlignment::Fill)
 						       .layout(DockLayout::create ())
-						       .background(ImagePaint (QUrl("asset:///background-page-title.amd")));
-	labelContainer->setLeftPadding (10);
-	labelContainer->setTopMargin (0);
+						       .background(Color::DarkGray);
 	labelContainer->setMinHeight (70);
+	labelContainer->setTopMargin (0);
 	labelContainer->add (_label);
-
-	/* To fit bottom gray title background */
-	rootContainer->setLeftPadding (2);
-	rootContainer->setRightPadding (3);
-	rootContainer->setBottomPadding (2);
 
 	rootContainer->add (imgContainer);
 	rootContainer->add (labelContainer);
@@ -96,7 +87,7 @@ NSRLastDocItem::activate (bool activate)
 	if (activate)
 		rootContainer->setBackground (Color::fromRGBA (0, 0.66, 0.87, 1.0));
 	else
-		rootContainer->setBackground (ImagePaint (QUrl ("asset:///border-gray.amd")));
+		rootContainer->setBackground (Color::fromRGBA (0.2, 0.2, 0.2, 1.0));
 }
 
 
