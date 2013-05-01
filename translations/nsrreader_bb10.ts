f8b157ed57817868f0beae608a95796f69e71a33
Alexander Saprykin <xelfium@gmail.com>
2013-05-01 20:15:30 +0400

    Update translation file

--- translations/nsrreader_bb10.ts
+++ translations/nsrreader_bb10.ts
@@ -4,12 +4,17 @@
 <context>
     <name>NSRAboutPage</name>
     <message>
+        <source>About</source>
+        <comment>About a program, window title</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>If you have any questions or suggestions (or even want to know what does NSR mean), do not hesitate to write me: &lt;a href=&apos;mailto:nsr.reader@gmail.com&apos;&gt;nsr.reader@gmail.com&lt;/a&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>About</source>
-        <comment>About a program, window title</comment>
+        <source>Quick Start Guide</source>
+        <comment>Manual for program usage</comment>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -21,7 +26,27 @@
     </message>
 </context>
 <context>
+    <name>NSRLastDocItemFactory</name>
+    <message>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Share</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove from List</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>NSRLastDocsPage</name>
+    <message>
+        <source>No recent documents</source>
+        <comment>List of recently used files is empty</comment>
+        <translation type="unfinished"></translation>
+    </message>
     <message>
         <source>Recent Documents</source>
         <comment>Title for window with recently opened documents</comment>
@@ -46,12 +71,12 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Use Column View to read documents with large amount of text data.</source>
+        <source>Use Text Reflow to read documents with large amount of text data.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Column View</source>
-        <comment>Option in preferences, reflows PDF in single column</comment>
+        <source>Text Reflow</source>
+        <comment>Option in preferences, reflows PDF in single text column</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -108,16 +133,22 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <source>About</source>
+        <comment>About a program, window title</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Share</source>
+        <comment>Share document between users</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Select File</source>
         <comment>Open file window</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Enter page (1 - %1):</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Enter password:</source>
+        <source>Selected file is already opened.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -126,11 +157,24 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <source>Enter page (1 - %1):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You are using text reflow the first time. Note that document formatting may be differ than in original one, no images displayed and page can be empty if there is no text in the document. Also text may not be displayed properly if appropriate language is not supported by phone.</source>
+        <comment>Text reflow is a view mode of PDF/DjVu document when only text information without images is displayed with word wrap feature enabled.</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Enter Password</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Seems that entered password is wrong.
+        <source>Enter password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Seems that entered password is wrong or encryption algorithm is not supported.
 Please do not blame hard NSR Reader for that because it is only a piece of software :)</source>
         <translation type="unfinished"></translation>
     </message>
@@ -139,9 +183,21 @@ Please do not blame hard NSR Reader for that because it is only a piece of softw
 NSR Reader tried open this file, but it can&apos;t :( Please check this file on desktop computer.</source>
         <translation type="unfinished"></translation>
     </message>
+</context>
+<context>
+    <name>NSRWelcomeView</name>
     <message>
-        <source>About</source>
-        <comment>About a program, window title</comment>
+        <source>Open</source>
+        <comment>Open document</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recent Documents</source>
+        <comment>Recently opened documents</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start reading</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
