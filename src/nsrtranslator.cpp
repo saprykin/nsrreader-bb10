2e72580acea0b60e8f7a0550f75c230fb93a0fb8
Alexander Saprykin <xelfium@gmail.com>
2013-10-17 22:17:20 +0400

    Add NSRTranslator to handle auto-translation on language change

---
+++ src/nsrtranslator.cpp
@@ -0,0 +1,156 @@
+#include "nsrtranslator.h"
+
+#include <bb/cascades/Label>
+#include <bb/cascades/ActionItem>
+#include <bb/cascades/Button>
+#include <bb/cascades/ActionSet>
+#include <bb/cascades/Option>
+#include <bb/cascades/MultiSelectHandler>
+#include <bb/cascades/TitleBar>
+
+#include <bbndk.h>
+
+using namespace bb::cascades;
+
+NSRTranslator::NSRTranslator (QObject *parent) :
+	QObject (parent)
+{
+}
+
+NSRTranslator::~NSRTranslator ()
+{
+	QList<NSRTranslatorInfo *> list;
+
+	while (!list.isEmpty ())
+		delete list.takeFirst ();
+}
+
+void
+NSRTranslator::addTranslatable (bb::cascades::UIObject*	obj,
+				NSRTranslatorType 	type,
+				const QString&		context,
+				const QString& 		text,
+				const QString&		disamb)
+{
+	NSRTranslatorInfo *info;
+
+	if (obj == NULL)
+		return;
+
+	removeTranslatable (obj);
+
+	info = new NSRTranslatorInfo ();
+	info->type = type;
+	info->context = context;
+	info->text = text;
+	info->disamb = disamb;
+
+	_hash.insert (obj, info);
+}
+
+void
+NSRTranslator::removeTranslatable (bb::cascades::UIObject* obj)
+{
+	if (obj == NULL)
+		return;
+
+	NSRTranslatorInfo *info = _hash.take (obj);
+
+	if (info != NULL)
+		delete info;
+}
+
+void
+NSRTranslator::translate ()
+{
+	QList<UIObject *> list = _hash.keys ();
+	int count = list.count ();
+
+	for (int i = 0; i < count; ++i) {
+		NSRTranslatorInfo *info = _hash.value (list.at (i));
+		QString translated = QCoreApplication::translate (info->context.toAscii().data (),
+								  info->text.toAscii().data (),
+								  info->disamb.toAscii().data ());
+
+		switch (info->type) {
+		case NSR_TRANSLATOR_TYPE_LABEL:
+		{
+			Label *label = dynamic_cast<Label *> (list.at (i));
+
+			if (label != NULL)
+				label->setText (translated);
+		}
+		break;
+		case NSR_TRANSLATOR_TYPE_BUTTON:
+		{
+			Button *button = dynamic_cast<Button *> (list.at (i));
+
+			if (button != NULL)
+				button->setText (translated);
+		}
+		break;
+		case NSR_TRANSLATOR_TYPE_ACTION:
+		{
+			ActionItem *action = dynamic_cast<ActionItem *> (list.at (i));
+
+			if (action != NULL)
+				action->setTitle (translated);
+		}
+		break;
+		case NSR_TRANSLATOR_TYPE_ACTIONSET_TITLE:
+		{
+			ActionSet *actionSet = dynamic_cast<ActionSet *> (list.at (i));
+
+			if (actionSet != NULL)
+				actionSet->setTitle (translated);
+		}
+		break;
+		case NSR_TRANSLATOR_TYPE_ACTIONSET_SUBTITLE:
+		{
+			ActionSet *actionSet = dynamic_cast<ActionSet *> (list.at (i));
+
+			if (actionSet != NULL)
+				actionSet->setSubtitle (translated);
+		}
+		break;
+		case NSR_TRANSLATOR_TYPE_OPTION:
+		{
+			Option *option = dynamic_cast<Option *> (list.at (i));
+
+			if (option != NULL)
+				option->setText (translated);
+		}
+		break;
+		case NSR_TRANSLATOR_TYPE_A11Y:
+		{
+#ifdef BBNDK_VERSION_AT_LEAST
+#  if BBNDK_VERSION_AT_LEAST(10,2,0)
+			AbstractA11yObject *a11y = dynamic_cast<AbstractA11yObject *> (list.at (i));
+
+			if (a11y != NULL)
+				a11y->setName (translated);
+#  endif
+#endif
+		}
+		break;
+		case NSR_TRANSLATOR_TYPE_SELECTHDL:
+		{
+			MultiSelectHandler *hdl = dynamic_cast<MultiSelectHandler *> (list.at (i));
+
+			if (hdl != NULL)
+				hdl->setStatus (translated);
+		}
+		break;
+		case NSR_TRANSLATOR_TYPE_TITLEBAR:
+		{
+			TitleBar *bar = dynamic_cast<TitleBar *> (list.at (i));
+
+			if (bar != NULL)
+				bar->setTitle (translated);
+		}
+		break;
+		default:
+			break;
+		}
+	}
+}
---
+++ src/nsrtranslator.h
@@ -0,0 +1,52 @@
+#ifndef NSRTRANSLATOR_H_
+#define NSRTRANSLATOR_H_
+
+#include <QObject>
+#include <QHash>
+#include <QString>
+
+#include <bb/cascades/UIObject>
+
+class NSRTranslator : public QObject
+{
+	Q_OBJECT
+	Q_ENUMS (NSRTranslatorType);
+public:
+	NSRTranslator (QObject *parent = 0);
+	virtual ~NSRTranslator ();
+
+	enum NSRTranslatorType {
+		NSR_TRANSLATOR_TYPE_LABEL		= 0,
+		NSR_TRANSLATOR_TYPE_BUTTON		= 1,
+		NSR_TRANSLATOR_TYPE_ACTION		= 2,
+		NSR_TRANSLATOR_TYPE_ACTIONSET_TITLE	= 3,
+		NSR_TRANSLATOR_TYPE_ACTIONSET_SUBTITLE	= 4,
+		NSR_TRANSLATOR_TYPE_OPTION		= 5,
+		NSR_TRANSLATOR_TYPE_A11Y		= 6,
+		NSR_TRANSLATOR_TYPE_SELECTHDL		= 7,
+		NSR_TRANSLATOR_TYPE_TITLEBAR		= 8
+	};
+
+	void addTranslatable (bb::cascades::UIObject 	*obj,
+			      NSRTranslatorType 	type,
+			      const QString&		context,
+			      const QString&		text,
+			      const QString&		disamb = QString ());
+	void removeTranslatable (bb::cascades::UIObject *obj);
+
+public Q_SLOTS:
+	void translate ();
+
+private:
+	struct NSRTranslatorInfo
+	{
+		QString			context;
+		QString			text;
+		QString			disamb;
+		NSRTranslatorType	type;
+	};
+
+	QHash<bb::cascades::UIObject *, NSRTranslatorInfo *>	_hash;
+};
+
+#endif /* NSRTRANSLATOR_H_ */
