d961076a7413e7dac2c10a8e9297bbadb31789c3
Alexander Saprykin <xelfium@gmail.com>
2013-11-04 19:52:24 +0400

    Fix bug when controls can be disabled if page was taken from cache

--- src/nsrreaderbb10.cpp
+++ src/nsrreaderbb10.cpp
@@ -643,6 +643,8 @@ NSRReaderBB10::onPageRendered (int number)
 			_pageView->fitToWidth (NSRRenderedPage::NSR_RENDER_REASON_CROP_TO_WIDTH);
 	}
 
+	updateVisualControls ();
+
 	if (_isActiveFrame)
 		onThumbnail ();
 }
@@ -818,7 +820,6 @@ void
 NSRReaderBB10::onIndicatorStopped ()
 {
 	_indicator->setVisible (false);
-	updateVisualControls ();
 }
 
 void
