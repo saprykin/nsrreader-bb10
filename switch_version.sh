48f94c662d3147d15e6d3bc4bf7b5c2965a9bbed
Alexander Saprykin <xelfium@gmail.com>
2014-03-12 23:55:01 +0400

    Add Indonesian translation

--- switch_version.sh
+++ switch_version.sh
@@ -24,7 +24,9 @@ mv ./.project.$CUR_SUFFIX ./.project
 if [[ "$CUR_SUFFIX" != "lite" ]]; then
 	mv ./translations/nsrreader_bb10_lite.ts ./translations/nsrreader_bb10.ts
 	mv ./translations/nsrreader_bb10_lite_ru.ts ./translations/nsrreader_bb10_ru.ts
+	mv ./translations/nsrreader_bb10_lite_id.ts ./translations/nsrreader_bb10_id.ts
 else
 	mv ./translations/nsrreader_bb10.ts ./translations/nsrreader_bb10_lite.ts
 	mv ./translations/nsrreader_bb10_ru.ts ./translations/nsrreader_bb10_lite_ru.ts
+	mv ./translations/nsrreader_bb10_id.ts ./translations/nsrreader_bb10_lite_id.ts
 fi
