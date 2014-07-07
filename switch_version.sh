3d608935ab1c2058af635a8a5aa6f5c2736ce216
Alexander Saprykin <xelfium@gmail.com>
2014-07-07 23:01:08 +0400

    Update switch_version.sh script to select SDK verson

--- switch_version.sh
+++ switch_version.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+if [[ $# -ne 1 ]]; then
+	echo "Usage: switch_version.sh [10.0 10.3]"
+	exit 0
+fi
+
+if [[ "$1" != "10.0" ]] && [[ "$1" != "10.3" ]]; then
+	echo "Only 10.0 and 10.3 SDK versions supported!"
+	exit 0
+fi
+
 if [[ -f Makefile.full ]]; then
 	NEW_SUFFIX=lite
 	CUR_SUFFIX=full
@@ -7,15 +17,19 @@ else
 	CUR_SUFFIX=lite
 fi
 
+if [[ -f ./bar-descriptor.xml ]]; then
+	rm -rf ./bar-descriptor.xml
+fi
+
+cp ./projects/$1/bar-descriptor.xml.$CUR_SUFFIX ./bar-descriptor.xml
+
 #Move current files to new prefix
-mv ./bar-descriptor.xml ./bar-descriptor.xml.$NEW_SUFFIX
 mv ./Makefile ./Makefile.$NEW_SUFFIX
 mv ./translations/Makefile ./translations/Makefile.$NEW_SUFFIX
 mv ./.cproject ./.cproject.$NEW_SUFFIX
 mv ./.project ./.project.$NEW_SUFFIX
 
 #Recover saved files
-mv ./bar-descriptor.xml.$CUR_SUFFIX ./bar-descriptor.xml
 mv ./Makefile.$CUR_SUFFIX ./Makefile
 mv ./translations/Makefile.$CUR_SUFFIX ./translations/Makefile
 mv ./.cproject.$CUR_SUFFIX ./.cproject
