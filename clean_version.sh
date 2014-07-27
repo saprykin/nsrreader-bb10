789e3e9139e5b71205aec57a5f1fe25a83452028
Alexander Saprykin <xelfium@gmail.com>
2014-07-27 20:43:30 +0400

    Add script to clean unversioned files

---
+++ clean_version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf ./bar-descriptor.xml
+rm -rf ./Makefile
+rm -rf ./translations/Makefile
+rm -rf ./translations/*.ts
+rm -rf ./translations/*.qm
+rm -rf ./x86
+rm -rf ./arm
+rm -rf ./assets/lib/*.so
