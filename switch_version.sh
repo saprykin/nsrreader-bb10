f2496b001ee89ed5563b7bda9d212117aaf8b314
Alexander Saprykin <xelfium@gmail.com>
2014-07-27 20:42:51 +0400

    Reorganize project files

--- switch_version.sh
+++ switch_version.sh
@@ -1,46 +1,43 @@
 #!/bin/bash
-if [[ $# -ne 1 ]]; then
-	echo "Usage: switch_version.sh [10.0 10.3]"
-	exit 0
+if [[ $# -ne 2 ]]; then
+	echo "Usage: switch_version.sh [10.0 10.1 10.2 10.3] [full lite]"
+	exit -1
 fi
 
-if [[ "$1" != "10.0" ]] && [[ "$1" != "10.3" ]]; then
-	echo "Only 10.0 and 10.3 SDK versions supported!"
-	exit 0
+if [[ ! -f ./nsrreader_bb10.pro ]]; then
+	echo "You should run this script in NSR Reader root directory"
+	exit -1
 fi
 
-if [[ -f Makefile.full ]]; then
-	NEW_SUFFIX=lite
-	CUR_SUFFIX=full
+if [[ "$1" != "10.0" ]] && [[ "$1" != "10.1" ]] && [[ "$1" != "10.2" ]] && [[ "$1" != "10.3" ]]; then
+	echo "Only 10.0, 10.1, 10.2 and 10.3 SDK versions supported!"
+	exit -1
+fi
+
+if [[ "$2" != "full" ]] && [[ "$2" != "lite" ]]; then
+	echo "Only full and lite app versions supported!"
+	exit -1
+fi
+
+rm -rf ./bar-descriptor.xml
+rm -rf ./Makefile
+rm -rf ./translations/Makefile
+rm -rf ./translations/*.ts
+
+if [[ "$1" == "10.3" ]]; then
+	PROJECT_SDK=10.3
 else
-	NEW_SUFFIX=full
-	CUR_SUFFIX=lite
+	PROJECT_SDK=10.0
 fi
 
-if [[ -f ./bar-descriptor.xml ]]; then
-	rm -rf ./bar-descriptor.xml
-fi
+cp ./projects/$PROJECT_SDK/bar-descriptor.xml.$2 ./bar-descriptor.xml
+cp ./projects/Makefile.$2 ./Makefile
+cp ./projects/translations/Makefile.$2 ./translations/Makefile
 
-cp ./projects/$1/bar-descriptor.xml.$CUR_SUFFIX ./bar-descriptor.xml
-
-#Move current files to new prefix
-mv ./Makefile ./Makefile.$NEW_SUFFIX
-mv ./translations/Makefile ./translations/Makefile.$NEW_SUFFIX
-mv ./.cproject ./.cproject.$NEW_SUFFIX
-mv ./.project ./.project.$NEW_SUFFIX
-
-#Recover saved files
-mv ./Makefile.$CUR_SUFFIX ./Makefile
-mv ./translations/Makefile.$CUR_SUFFIX ./translations/Makefile
-mv ./.cproject.$CUR_SUFFIX ./.cproject
-mv ./.project.$CUR_SUFFIX ./.project
-
-if [[ "$CUR_SUFFIX" != "lite" ]]; then
-	mv ./translations/nsrreader_bb10_lite.ts ./translations/nsrreader_bb10.ts
-	mv ./translations/nsrreader_bb10_lite_ru.ts ./translations/nsrreader_bb10_ru.ts
-	mv ./translations/nsrreader_bb10_lite_id.ts ./translations/nsrreader_bb10_id.ts
-else
-	mv ./translations/nsrreader_bb10.ts ./translations/nsrreader_bb10_lite.ts
-	mv ./translations/nsrreader_bb10_ru.ts ./translations/nsrreader_bb10_lite_ru.ts
-	mv ./translations/nsrreader_bb10_id.ts ./translations/nsrreader_bb10_lite_id.ts
-fi
+for ts in `(ls ./projects/translations/ | grep .*.ts)`; do
+	if [[ "$2" == "lite" ]]; then
+		cp ./projects/translations/$ts ./translations/`(echo $ts | sed s/nsrreader_bb10/nsrreader_bb10_lite/)`
+	else
+		cp ./projects/translations/$ts ./translations/$ts
+	fi
+done
