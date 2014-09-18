974f293f6b517b8594b5230c92692e6c24823099
Alexander Saprykin <xelfium@gmail.com>
2014-09-18 23:23:10 +0400

    Only setup visual style before app launch on OS < 10.3

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include <bb/cascades/Application>
 
 #include "nsrreaderbb10.h"
+#include "nsrreader.h"
+
+#if !defined (BBNDK_VERSION_AT_LEAST) || !BBNDK_VERSION_AT_LEAST(10,3,0)
 #include "nsrsettings.h"
+#endif
 
 using namespace bb::cascades;
 
 Q_DECL_EXPORT int main (int argc, char **argv)
 {
+#if !defined (BBNDK_VERSION_AT_LEAST) || !BBNDK_VERSION_AT_LEAST(10,3,0)
 	if (NSRSettings::instance()->getVisualStyle () == VisualStyle::Dark)
 		qputenv ("CASCADES_THEME", "dark");
 	else
 		qputenv ("CASCADES_THEME", "bright");
+#endif
 
 	Application app (argc, argv);
 
