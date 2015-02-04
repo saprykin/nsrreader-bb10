1c7a361e433f7784a4577572ab12c10cf825dace
Alexander Saprykin <xelfium@gmail.com>
2015-02-04 21:02:04 +0300

    Add NSRReadingTheme class with text theme types

---
+++ src/nsrreadingtheme.cpp
@@ -0,0 +1,1 @@
+#include "nsrreadingtheme.h"
---
+++ src/nsrreadingtheme.h
@@ -0,0 +1,17 @@
+#ifndef NSRREADINGTHEME_H_
+#define NSRREADINGTHEME_H_
+
+class NSRReadingTheme
+{
+public:
+	enum Type {
+		Normal	 = 1,
+		Sepia	 = 2
+	};
+
+private:
+	NSRReadingTheme () {}
+	~NSRReadingTheme () {}
+};
+
+#endif /* NSRREADINGTHEME_H_ */
